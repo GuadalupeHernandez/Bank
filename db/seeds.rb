bc22be95e5c868fe58dd3b35aa1d6ad4481fb065
Guadalupe <lupita.hernandeztovar@gmail.com>
2017-08-24 11:44:18 -0500

    Lool3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-clients = clients.create(first_name: 'Guadalupe', last_name: 'Hernandez', middle_name: 'Lupita', client_numer:1)
+lupita = Clients.create(first_name: 'Guadalupe', last_name: 'Hernandez', middle_name: 'Lupita', client_numer:1)
